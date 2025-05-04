2b5f6905c031c2802216106165ef8cac48119567
andrea <andrea@Andrea.>
2025-05-04 20:52:48 +0200

    Add led blinky code

--- 2-devicetree-overlay/blinky/src/main.c
+++ 2-devicetree-overlay/blinky/src/main.c
@@ -4,22 +4,49 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
+#include <zephyr/kernel.h>
 #include <zephyr/device.h>
 #include <zephyr/drivers/gpio.h>
 
 /* 1000 msec = 1 sec */
-#define SLEEP_TIME_MS   1000
+#define SLEEP_TIME_MS 500
 
 /* The devicetree node identifier for the "led0" alias. */
-#define LED0_NODE DT_NODELABEL(green_led_0)
+#define LED0_NODE DT_NODELABEL(gpio0)
 
 /*
  * A build error on this line means your board is unsupported.
  * See the sample documentation for information on how to fix this.
  */
-static const struct device *gpionamw = DEVICE_DT_GET(LED0_NODE);
+static const struct device *led = DEVICE_DT_GET(LED0_NODE);
 
 int main(void)
 {
+	int ret;
+	bool led_state = true;
+
+	if (!device_is_ready(led))
+	{
+		return 0;
+	}
+
+	ret = gpio_pin_configure(led, 2, GPIO_OUTPUT_ACTIVE);
+	if (ret < 0)
+	{
+		return 0;
+	}
+
+	while (1)
+	{
+		ret = gpio_pin_toggle(led, 2);
+		if (ret < 0)
+		{
+			return 0;
+		}
+
+		led_state = !led_state;
+		printf("LED state: %s\n", led_state ? "ON" : "OFF");
+		k_msleep(SLEEP_TIME_MS);
+	}
 	return 0;
 }
